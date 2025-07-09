4098b2182daf1cf794432921289c8cebdb8cdd1d
Oliwier Posiakow <oliwierposiek@gmail.com>
2025-07-09 22:03:55 +0200

    Modal, images layout
    - implemented modal + interactions
    - fixed gallery images placement
    - added interactivity to project via TS

---
+++ ts/script.ts
@@ -0,0 +1,26 @@
+//HTML Elements
+const body = document.querySelector('body');
+const galleryButtons = document.querySelectorAll('.gallery__item');
+
+//Modal items
+const modal: HTMLDialogElement = document.querySelector('#modal');
+const modalCloseButton: HTMLButtonElement =
+  document.querySelector('#modal-close');
+
+//For each gallery item, add an event listener to open the modal, and disable body scroll
+galleryButtons.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (!modal.open) {
+      modal.showModal();
+      body.classList.add('scroll-disabled');
+    }
+  });
+});
+
+//Add an event listener to the modal close button to close the modal and enable body scroll
+modalCloseButton.addEventListener('click', () => {
+  if (modal.open) {
+    modal.close();
+    body.classList.remove('scroll-disabled');
+  }
+});
